b48fb7c6ca88c3b55dc7c7848a9cfab2882fa701
Ezequix <116773393+snappiermars@users.noreply.github.com>
2023-02-13 12:34:32 -0600

    Comentarios en el código

--- sumaDosNumeros.py
+++ sumaDosNumeros.py
@@ -1,5 +1,8 @@
+#Lectura de la primera variable
 A = input("Escribe el primer numero: ")
+#Lectura de la segunda variable
 B = input("Escribe el segundo numero: ")
-
+#Suma de las dos variables
 C = A + B
+#Impresion de la variable C
 print (C)
